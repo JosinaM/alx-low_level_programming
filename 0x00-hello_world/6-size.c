ed606c4f01749eeebeeb78b1479c976dd13883ee
Josina <kmafodi@yahoo.com>
2023-04-20 15:54:43 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 
-        char c;
-        int i;
-        long int l;
-        long long int d;
-        float f;
+	char c;
+	int i;
+	long int l;
+	long long int d;
+	float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
