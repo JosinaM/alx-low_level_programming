f4e46111b185a80a30ceb19963f0fc72d89ecaf7
Josina <kmafodi@yahoo.com>
2023-04-20 14:49:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts ("Programming is like building a multilingual puzzle"); return 0;
+#include <stdio.h>
+/*
+ * using function puts to output a string
+ * Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
