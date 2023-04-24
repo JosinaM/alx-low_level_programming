a1a693b094e70f0651325d67face1ef00e84951e
Josina <kmafodi@yahoo.com>
2023-04-24 10:11:29 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Descriptions: lists lowercase alphabets except q and e
+ * Return: always 0
+ */
+int main(void)
+{
+	char alpha = a;
+
+	while (alpha <= z)
+	{
+		if (alpha != 'e' && alpha != 'q')
+	{
+		putchar(alpha);
+	}
+		alpha++;
+	}
+	putchar(\n);
+	return	(0);
+}
